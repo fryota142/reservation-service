4dd512ee73d8668cc26e495f5f5f07dc42c557e5
fryota142 <anhcyr9771@gmail.com>
2020-02-21 10:59:20 +0900

    add validation email max_length. delete unnecessary file.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 15 }
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, length: { maximum: 64 }
   has_secure_password
 end
