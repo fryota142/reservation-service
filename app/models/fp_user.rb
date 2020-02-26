9189e3424c1594994b0550621371d7c74c74db28
fryota142 <anhcyr9771@gmail.com>
2020-02-26 10:22:17 +0900

    add reservation controller.

--- app/models/fp_user.rb
+++ app/models/fp_user.rb
@@ -2,4 +2,6 @@ class FpUser < ApplicationRecord
   validates :name, presence: true, length: { maximum: 15 }
   validates :email, presence: true, uniqueness: true, length: { maximum: 64 }
   has_secure_password
+
+  has_one :reservation
 end
