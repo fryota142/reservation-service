daffeb966d1487cbf821e36e0b590e543a24ca7c
Ryota Fukata <fukata@haw.co.jp>
2020-03-02 06:02:45 +0900

    implement calendar for user and fp_user.
    ユーザとFPで別のカレンダーを表示するようにした。
    FPは時間をクリックした際に予約枠が作成できるようにしており、ユーザは予約枠の確保だけできるようにした

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,7 +20,8 @@
 //= require turbolinks
 //= require_tree .
 
-$(document).ready(function() {
+$(function () {
+  $(document).on('turbolinks:load', function () {
 
     create_event = function(start){
       $.ajaxPrefilter(function(options, originalOptions, jqXHR) {
@@ -45,40 +46,92 @@ $(document).ready(function() {
       });
     };
 
-    $('#calendar').fullCalendar({
-      header: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'month,agendaWeek,agendaDay'
-      },
-      eventColor: '#63ceef',
-      minTime: "10:00:00",
-      maxTime: "18:00:00",
-      defaultTimedEventDuration: '10:00:00',
-      slotDuration: '00:30:00',
-      snapDuration: '00:30:00',
-      navLinks: true,
-      selectable: true,
-      selectHelper: true,
-      select: function(start, end) {
-        var title = prompt('イベントを追加');
-        var eventData;
-        if (title) {
-          eventData = {
-            title: title,
-            start: start
-          };
-          $('#calendar').fullCalendar('renderEvent', eventData, true);
-          $('#calendar').fullCalendar('unselect');
-          create_event(start);
-        }
-      },
-      timezone: 'UTC',
-      events: '/events.json',
-      editable: true,
-      eventClick: function(info) {
-        window.location.href = ('/reservations/' + info.id);
-      }
-    });
+    if ($('#fp_calendar').length) {
+      function eventCalendar() {
+          return $('#fp_calendar').fullCalendar({
+          });
+      };
+      function clearCalendar() {
+          $('#fp_calendar').html('');
+      };
 
+      $(document).on('turbolinks:load', function () {
+          eventCalendar();
+      });
+      $(document).on('turbolinks:before-cache', clearCalendar);
+
+      $('#fp_calendar').fullCalendar({
+        header: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'month,agendaWeek,agendaDay'
+        },
+        eventColor: '#63ceef',
+        minTime: "10:00:00",
+        maxTime: "18:00:00",
+        defaultTimedEventDuration: '00:30:00',
+        slotDuration: '00:30:00',
+        snapDuration: '00:30:00',
+        navLinks: true,
+        selectable: true,
+        selectHelper: true,
+        select: function(start, end, view) {
+          if(view.name != 'month') {
+            var eventData;
+            if (window.confirm('この時間に予約枠を作成しますか?')) {
+              eventData = {
+                start: start
+              };
+              $('#fp_calendar').fullCalendar('renderEvent', eventData, true);
+              $('#fp_calendar').fullCalendar('unselect');
+              create_event(start);
+            }
+          };
+        },
+        events: '/events.json',
+        eventClick: function(info) {
+          window.location.href = ('/reservations/' + info.id);
+        },
+      });
+    }
+
+    if ($('#user_calendar').length) {
+      function eventCalendar() {
+          return $('#user_calendar').fullCalendar({
+          });
+      };
+      function clearCalendar() {
+          $('#user_calendar').html('');
+      };
+
+      $(document).on('turbolinks:load', function () {
+          eventCalendar();
+      });
+      $(document).on('turbolinks:before-cache', clearCalendar);
+      $('#user_calendar').fullCalendar({
+        header: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'month,agendaWeek,agendaDay'
+        },
+        eventColor: '#63ceef',
+        minTime: "10:00:00",
+        maxTime: "18:00:00",
+        defaultTimedEventDuration: '00:30:00',
+        slotDuration: '00:30:00',
+        snapDuration: '00:30:00',
+        navLinks: true,
+        dayClick: function(date, jsEvent, view) {
+          if(view.name == 'month' || view.name == 'agendaWeek') {
+            $('#user_calendar').fullCalendar('changeView', 'agendaDay');
+            $('#user_calendar').fullCalendar('gotoDate', date);
+          }
+        },
+        events: '/events.json',
+        eventClick: function(info) {
+          window.location.href = ('/reservations/' + info.id);
+        },
+      });
+    }
+  });
 });
