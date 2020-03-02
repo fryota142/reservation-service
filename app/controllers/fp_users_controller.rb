137bfe24c41f89c2b9cefe9117a5fa56cf68e92e
fryota142 <anhcyr9771@gmail.com>
2020-03-02 11:37:08 +0900

    add flash message and readme.
    flashのメッセージを日本語に変更した
    crud時にflashでメッセージが出るようにした
    readmeの内容を変更した

--- app/controllers/fp_users_controller.rb
+++ app/controllers/fp_users_controller.rb
@@ -10,8 +10,10 @@ class FpUsersController < ApplicationController
     @fp_user = FpUser.new(fpuser_params)
     if @fp_user.save
       log_in @fp_user
+      flash[:success] = "FPユーザを作成しました"
       redirect_to @fp_user
     else
+      flash[:danger] = "FPユーザを作成できませんでした"
       render 'new'
     end
   end
@@ -20,20 +22,22 @@ class FpUsersController < ApplicationController
     @fp_user = FpUser.find(params[:id])
     @reservations = Reservation.all.where(fp_user_id: @fp_user.id)
   rescue ActiveRecord::RecordNotFound => e
-    redirect_to :root, alert: 'User not found'
+    redirect_to :root, alert: 'ユーザが見つかりませんでした'
   end
 
   def edit
     @fp_user = FpUser.find(params[:id])
   rescue ActiveRecord::RecordNotFound => e
-    redirect_to :root, alert: 'User not found'
+    redirect_to :root, alert: 'ユーザが見つかりませんでした'
   end
 
   def update
     @fp_user = FpUser.find(params[:id])
     if @fp_user.update(fpuser_params)
+      flash[:success] = "ユーザ情報を更新しました"
       redirect_to @fp_user
     else
+      flash[:danger] = "ユーザ情報を更新できませんでした"
       render 'edit'
     end
   end
@@ -51,7 +55,7 @@ class FpUsersController < ApplicationController
 
   def require_login
     unless logged_in?
-      flash[:danger] = "Please log in."
+      flash[:danger] = "ログインしてください"
       redirect_to login_url
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,10 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
+      flash[:success] = "ユーザを作成しました"
       redirect_to @user
     else
+      flash[:danger] = "ユーザの作成に失敗しました"
       render 'new'
     end
   end
@@ -20,20 +22,22 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @reservations = Reservation.all.where(user_id: @user.id)
   rescue ActiveRecord::RecordNotFound => e
-    redirect_to :root, alert: 'User not found'
+    redirect_to :root, alert: 'ユーザが見つかりません'
   end
 
   def edit
     @user = User.find(params[:id])
   rescue ActiveRecord::RecordNotFound => e
-    redirect_to :root, alert: 'User not found'
+    redirect_to :root, alert: 'ユーザが見つかりません'
   end
 
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
+      flash[:success] = "ユーザ情報を更新しました"
       redirect_to @user
     else
+      flash[:danger] = "ユーザ情報を更新できませんでした"
       render 'edit'
     end
   end
@@ -51,7 +55,7 @@ class UsersController < ApplicationController
 
   def require_login
     unless logged_in?
-      flash[:danger] = "Please log in."
+      flash[:danger] = "ログインしてください"
       redirect_to login_url
     end
   end
